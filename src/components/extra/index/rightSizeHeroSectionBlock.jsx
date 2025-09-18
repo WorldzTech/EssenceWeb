8bf2c3a8ee1cec794a1dfaf662b2a174f6a84756
daniilbaikalov <felix.trof@gmail.com>
2025-09-18 22:26:16 +0300

    done

--- src/components/extra/index/rightSizeHeroSectionBlock.jsx
+++ src/components/extra/index/rightSizeHeroSectionBlock.jsx
@@ -39,7 +39,7 @@ export default function RightSideHeroSectionBlock() {
     }, [bgRef])
 
     return (
-        <div className="absolute end-0 top-0 aspect-square max-xl:bottom-0 max-xl:end-1/2 max-xl:translate-x-1/2 max-xl:translate-y-full">
+        <div className="absolute end-0 top-0 aspect-square max-xl:bottom-0 max-xl:end-1/2 max-xl:translate-x-1/2 max-xl:translate-y-full max-xl:hidden">
             <div className="relative w-128 h-64 bg-[#F6F4EE] rotate-45 border border-gray-100 rounded-full" ref={bgRef} style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.2))' }}>
               <SpinningStar className="absolute start-0 -translate-y-full z-50" size={75} duration={8}/>
               <SpinningStar className="absolute bottom-0 end-0 mt-10 translate-y-full rotate-45 z-50" size={110} duration={19} direction={-1}/>
